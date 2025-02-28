d448e233c6d5fead145d8fab4d13df65da620b2a
shiequinn <165054855+shiequinn@users.noreply.github.com>
2025-02-28 15:43:31 +0800

    Update booknow.js

--- js/booknow.js
+++ js/booknow.js
@@ -32,3 +32,18 @@ document.addEventListener("DOMContentLoaded", function () {
   });
     });
   });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+hamburger.classList.remove("active");
+navMenu.classList.remove("active");
+}
